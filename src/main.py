8b5684b43e1ac592d779c64c19be889883abf2b8
c0balt60 <elmt.c0balt60@gmail.com>
2025-02-28 11:55:29 -0500

    initial commit

---
+++ src/main.py
@@ -0,0 +1,16 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       andr6521                                                     #
+# 	Created:      2/28/2025, 11:52:16 AM                                       #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Component definitions
+brain = Brain()
+controller = Controller()
+
