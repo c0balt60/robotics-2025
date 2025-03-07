4d46be4db0b183145a6197a4b4f85b9e9fc61431
c0balt60 <elmt.c0balt60@gmail.com>
2025-03-07 11:34:33 -0500

    chore: created main file feat: added drive function

---
+++ src/main.py
@@ -0,0 +1,38 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       andr6521                                                     #
+# 	Created:      2/28/2025, 11:01:22 AM                                       #
+# 	Description:  Main branch containing base config for robots                #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+    
+# Component definitions
+brain = Brain()
+controller = Controller()
+
+# Motor definitions
+left_motor = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
+right_motor = Motor(Ports.PORT2, GearSetting.RATIO_18_1, True)
+
+# TODO: COnfigure Wheel base
+# DriveTrain class.
+drivetrain = DriveTrain(left_motor, right_motor, 319.19, 295, 40, MM, 1)
+
+
+def Drive(dir: DirectionType, velocity: int = 100, units: VelocityPercentUnits = PERCENT):
+    """
+    Function for handling drive mechanics
+
+    Args:
+        dir (DirectionType): _description_
+        velocity (int, optional): _description_. Defaults to 100.
+        units (VelocityPercentUnits, optional): _description_. Defaults to PERCENT.
+    """
+
+    # TODO: Add safe-guards for movement
+
+    drivetrain.drive(dir, velocity, units)
