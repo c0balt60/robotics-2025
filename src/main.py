1ec964d39f7d976c645ef6836f4562bf04857b1a
c0balt60 <elmt.c0balt60@gmail.com>
2025-03-21 12:15:17 -0400

    chore: added general purpose methods

--- src/main.py
+++ src/main.py
@@ -4,12 +4,13 @@
 # 	Author:       andr6521                                                     #
 # 	Created:      2/28/2025, 11:01:22 AM                                       #
 # 	Description:  Main branch containing base config for robots                #
+#   Bot:          ~general                                                     #
 #                                                                              #
 # ---------------------------------------------------------------------------- #
 
 # Library imports
 from vex import *
-    
+
 # Component definitions
 brain = Brain()
 controller = Controller()
@@ -22,8 +23,7 @@ right_motor = Motor(Ports.PORT2, GearSetting.RATIO_18_1, True)
 # DriveTrain class.
 drivetrain = DriveTrain(left_motor, right_motor, 319.19, 295, 40, MM, 1)
 
-
-def Drive(dir: DirectionType, velocity: int = 100, units: VelocityPercentUnits = PERCENT):
+def Drive(dir: DirectionType, velocity: int = 100, units: VelocityPercentUnits = PERCENT): #type: ignore
     """
     Function for handling drive mechanics
 
@@ -36,3 +36,11 @@ def Drive(dir: DirectionType, velocity: int = 100, units: VelocityPercentUnits =
     # TODO: Add safe-guards for movement
 
     drivetrain.drive(dir, velocity, units)
+
+def DriveMotors(right: Motor, left: Motor):
+    """_summary_
+
+    Args:
+        right (Motor): _description_
+        left (Motor): _description_
+    """
